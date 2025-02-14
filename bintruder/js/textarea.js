9abbc6bfd90ee776a58257268b23d4494743208e
Bencso <158219939+bencso@users.noreply.github.com>
2025-02-14 14:45:10 +0100

    Textarea refactoring

--- bintruder/js/textarea.js
+++ bintruder/js/textarea.js
@@ -2,14 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const textarea = document.getElementById('requestBody');
     const lineNumbersEle = document.getElementById('linenumbers');
 
-    const calculateLineNumbers = () => {
-        const lines = textarea.value.split('\n').length;
-        const lineNumbers = Array.from({ length: lines }, (_, i) => i + 1);
-
-        lineNumbersEle.innerHTML = lineNumbers.map((lineNumber) => `<div>${lineNumber || '&nbsp;'}</div>`).join('');
-        lineNumbersEle.style.height = `${textarea.scrollHeight}px`;
-    };
-
-    textarea.addEventListener('input', calculateLineNumbers);
-    textarea.addEventListener('blur', () => textarea.value = textarea.value.trim());
+    textarea.addEventListener('input',
+        () => lineNumbersEle.innerHTML = Array.from({ length: textarea.value.split('\n').length }, (_, i) => i + 1)
+        .map((lineNumber) => `<div>${lineNumber}</div>`).join(''));
 });
